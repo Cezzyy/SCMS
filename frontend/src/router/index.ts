cd320dce17115e9d3c9afc364c6932ebc08222fe
Karl <karllastimoso454@gmail.com>
2025-04-20 22:53:05 +0800

    feat(router): add dashboard route to the application

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -9,6 +9,11 @@ const router = createRouter({
       component: () => import('../views/LoginView.vue'),
     },
     {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('../views/HomeView.vue'),
+    },
+    {
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue'),
